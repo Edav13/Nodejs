222fde1687515de0d3ac9ebd37a0eff295c4e9aa
Edav13 <Edav13@hotmail.com>
2017-11-21 14:37:56 -0400

    Node0002

--- 01ModulosCore/01-single-thread.js
+++ 01ModulosCore/01-single-thread.js
@@ -1,19 +1,28 @@
 /*single thread*/
 'use strict'
 function singleThread() {
-	conosle.log('=====================================');
-	conosle.log('		EL PROCESO DE NODE.JS');
-	conosle.log('Id del proceso............. ' + process.pid);
-	conosle.log('Titulo..................... ' + process.title);
-	conosle.log('Directorio de Node......... ' + process.execPath);
-	conosle.log('Directorio actual.......... ' + process.cwd());
-	conosle.log('Versión de Node............ ' + process.version);
-	conosle.log('Versiones Dependencias..... ' + process.versions);
-	conosle.log('Plataforma (S.O.).......... ' + process.platform);
-	conosle.log('Arquitectura (S.O.)........ ' + process.arch);
-	conosle.log('Tiempo activo de Node...... ' + process.uptime());
-	conosle.log('Argumentos del proceso..... ' + process.argv);
-	conosle.log('--------------------------------------------');
+	process.argv.push('Estamos aprendiendo Node.js');
+	process.argv.push(19);
+	process.argv.push(null);
+	process.argv.push(true);
+
+	console.log('=====================================');
+	console.log('		EL PROCESO DE NODE.JS');
+	console.log('Id del proceso............. ' + process.pid);
+	console.log('Titulo..................... ' + process.title);
+	console.log('Directorio de Node......... ' + process.execPath);
+	console.log('Directorio actual.......... ' + process.cwd());
+	console.log('Versión de Node............ ' + process.version);
+	console.log('Versiones Dependencias..... ' + process.versions);
+	console.log('Plataforma (S.O.).......... ' + process.platform);
+	console.log('Arquitectura (S.O.)........ ' + process.arch);
+	console.log('Tiempo activo de Node...... ' + process.uptime());
+	console.log('Argumentos del proceso..... ' + process.argv);
+	console.log('--------------------------------------------');
+	console.log(process.argv[0], process.argv[1]);
+	process.argv.forEach(function(single){
+		console.log(single);
+	});
 }
 
 singleThread();
