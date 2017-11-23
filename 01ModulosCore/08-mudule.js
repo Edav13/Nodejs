15a75341cda9fb75e672d4a331d94aeafe460106
Edav13 <Edav13@hotmail.com>
2017-11-23 09:20:58 -0400

    Nodejs0008

---
+++ 01ModulosCore/08-mudule.js
@@ -0,0 +1,5 @@
+'use strict'
+
+var myData = require('./my-data.js');
+
+console.log(myData);
---
+++ 01ModulosCore/my-data.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var name = 'Edav',
+	email = 'edav13@hotmail.com',
+	_phone = '312654987';
+
+module.exports.name = name;
+module.exports.email = email;
